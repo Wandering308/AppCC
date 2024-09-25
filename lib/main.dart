54d07e771e99c6a1d89564f3e15a2f08c34ea7c8
Wandering308 <felipegm1799@gmail.com>
2024-09-25 17:25:49 -0500

    home

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,68 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.jpg'), // Usar ruta relativa
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Imagen del logo
+              Image.asset(
+                'assets/logo.png', // Usar ruta relativa
+                height: 150,
+              ),
+              SizedBox(height: 30),
+              // Texto de bienvenida
+              Text(
+                'Bienvenido\nIngrese para continuar',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 40),
+              // Botón de ingresar
+              ElevatedButton(
+                onPressed: () {
+                  // Acción al presionar el botón
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                ),
+                child: Text(
+                  'Ingresar',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
