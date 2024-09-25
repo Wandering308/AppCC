b26dd3cb57f7c7ba0469be1cfa31e82e4472c838
Wandering308 <felipegm1799@gmail.com>
2024-09-25 16:41:35 -0500

    Commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_app_cc
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_app_cc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_app_cc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
